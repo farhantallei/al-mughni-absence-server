727241a961e696d68a5eb52106ca6405e7dda73d
Farhan Pradana Tallei <11686694+farhantallei@users.noreply.github.com>
2023-01-19 23:27:42 +0700

    Delete dist directory

--- dist/utils/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.commitToDB = void 0;
-var commitToDB_1 = require("./commitToDB");
-Object.defineProperty(exports, "commitToDB", { enumerable: true, get: function () { return commitToDB_1.commitToDB; } });
